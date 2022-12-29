bc6255b4d512e26027e9d2d5d08445f6f34a28cd
Agacy junior <juniormartinssom@gmail.com>
2022-12-29 19:12:12 -0300

    Visual structure of the timetable

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,40 @@
 import { Calendar } from '../../../../../components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de dezembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
